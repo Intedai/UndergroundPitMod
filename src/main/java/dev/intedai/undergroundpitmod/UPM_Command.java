1c8c2e3cd6456b6aab47a29e660f32f5ecfe05fd
Intedai <69306633+Intedai@users.noreply.github.com>
2025-10-15 04:50:06 +0300

    Added the abillity to view Minor, Major or certain event added Event getters for isMajor and type

--- src/main/java/dev/intedai/undergroundpitmod/UPM_Command.java
+++ src/main/java/dev/intedai/undergroundpitmod/UPM_Command.java
@@ -1,5 +1,8 @@
 package dev.intedai.undergroundpitmod;
 
+import java.util.function.Predicate;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.CommandException;
@@ -8,6 +11,7 @@ import net.minecraft.util.ChatComponentText;
 
 public class UPM_Command extends CommandBase {
 	private Event[] events;
+	private final int EVENT_COUNT = 12;
 	
 	public UPM_Command()
 	{
@@ -26,47 +30,84 @@ public class UPM_Command extends CommandBase {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "\n/upm to see upcoming events\n/upm refresh to fetch api data.";
+    	StringBuilder sb = new StringBuilder("\n/upm to see upcoming events")
+    		.append("\n/upm <event name> to see every time the event starts")
+    		.append("\n/upm refresh to fetch api data")
+    		.append("\n/upm major to see upcoming major events")
+    		.append("\n/upm minor to see upcoming minor events");
+    	
+    	return sb.toString();
     }
     
     private void sendString(ICommandSender sender, String string)
     {
-    	StringBuilder sb = new StringBuilder();
-    	sb.append("§6§l------------§b§lUPM§6§l------------\n§r").append(string).append("§r§6§l---------------------------");
+    	StringBuilder sb = new StringBuilder("§6§l------------§b§lUPM§6§l------------\n§r")
+    		.append(string)
+    		.append("§r§6§l---------------------------");
     	
     	sender.addChatMessage(new ChatComponentText(sb.toString()));
     	
     }
+    
+    private String getEventsOnCondition(int count, Predicate<Event> condition)
+    {
+		StringBuilder sb = new StringBuilder();
+		
+		int counter = 0;
+		for(Event event : events)
+		{
+			if (event.displayEvent() && condition.test(event))
+			{
+				sb.append(event.toString()).append("\n");
+				counter++;
+			}
+			if (counter == count)
+				break;
+		}
+		
+		return sb.toString();
+    }
+    
     @Override
     public void processCommand(ICommandSender sender, String[] args) throws CommandException {
     	
     	if (args.length == 0)
-    	{    		
-    		StringBuilder sb = new StringBuilder();
-    		
-    		int count = 0;
-    		for(Event event : events)
-    		{
-    			if (event.displayEvent())
-    			{
-    				sb.append(event.toString()).append("\n");
-    				count++;
-    			}
-    			if (count > 12)
-    				break;
-    		}
-    		
-    		sendString(sender, sb.toString());
-    	}
-    	else if (args.length == 1 && args[0].equalsIgnoreCase("refresh"))
     	{
-    		refreshEvents();
-    		sendString(sender, "§aFetched data from BrookeAFK's API.\n");
+    		sendString(sender, getEventsOnCondition(EVENT_COUNT, event -> true));
+    		return;
     	}
+    	
     	else
     	{
-    		throw new WrongUsageException(getCommandUsage(sender));
+    		String joinedArgs = String.join(" ", args).trim();
+    		
+    		try // Check if user wanted to see specific event
+    		{
+    			EventType wantedEvent = EventType.getType(joinedArgs);
+    			sendString(sender, getEventsOnCondition(EVENT_COUNT, event -> event.getEventType() == wantedEvent));
+    			return;
+    		}
+    		catch (IllegalArgumentException e) // Args are not an event name
+    		{
+    			if (args.length == 1)
+    			{			
+    				switch(args[0].toLowerCase())
+    				{
+	    				case "refresh":
+	    					refreshEvents();
+	    					sendString(sender, "§aFetched data from BrookeAFK's API.\n");
+	    					return;
+	    				case "major":
+	    					sendString(sender, getEventsOnCondition(EVENT_COUNT, event -> event.getIsMajor()));
+	    					return;
+	    				case "minor":
+	    					sendString(sender, getEventsOnCondition(EVENT_COUNT, event -> !event.getIsMajor()));
+	    					return;
+    				}
+    			}
+    		}
     	}
+    	throw new WrongUsageException(getCommandUsage(sender));
     }
     
     @Override
